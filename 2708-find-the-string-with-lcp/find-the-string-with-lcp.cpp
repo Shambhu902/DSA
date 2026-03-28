99e27640994d855fc6faf0d5598f8760179831ef
Shambhu <148757019+Shambhu902@users.noreply.github.com>
2026-03-28 22:10:40 +0530

    Time: 11 ms (60.24%) | Memory: 75.5 MB (66.27%) - LeetSync

--- 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
@@ -7,17 +7,15 @@ class Solution {
     vector<char> word(n, nonLetter);
 
     for (int i = 0; i < n; ++i) {
-      if (word[i] != nonLetter)  // There's a candidate already.
+      if (word[i] != nonLetter)  
         continue;
-      if (++c > 'z')  // Run out of letters, so return "".
+      if (++c > 'z')  
         return "";
-      // No need to consider [0..i - 1] since they were considered.
       for (int j = i; j < n; ++j)
         if (lcp[i][j] > 0)
           word[j] = c;
     }
 
-    // Check if `word` is valid.
     for (int i = 0; i < n; ++i)
       for (int j = 0; j < n; ++j) {
         const int nextLcp = i + 1 < n && j + 1 < n ? lcp[i + 1][j + 1] : 0;
