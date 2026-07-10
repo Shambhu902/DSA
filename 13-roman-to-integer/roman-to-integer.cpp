c7118774ee14bd3dba520b2438da6fbfd5f9e467
Shambhu <148757019+Shambhu902@users.noreply.github.com>
2026-07-10 14:37:32 +0530

    Time: 15 ms (9.67%) | Memory: 13.4 MB (17.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        // Rule:
-        // 'I'= 1, 'X' = 10,'C' = 100, 'M' = 1000, 'V' = 5, 'L' = 50, 'D' = 500;
-        // Adjacent(Left) >= Adjacent(Right): Right + Left;
-        // Adjacent(Left) < Adjacent(Right): Right - Left;
+        
         unordered_map<char,int>m({{'I',1}, {'X',10}, {'C',100}, {'M',1000}, {'V',5}, {'L',50}, {'D',500}});
         if(s.size() == 0) return 0;
         int sum = m[s[s.size() - 1]];
